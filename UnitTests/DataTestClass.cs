5d3d8142c827727ad61cb640afc43d820715b7dc
ChernV <vchern185@gmail.com>
2023-01-22 22:31:06 +0300

    UnitTests.DataTestClass minor fix

--- UnitTests/DataTestClass.cs
+++ UnitTests/DataTestClass.cs
@@ -1,11 +1,14 @@
+using Boolean = CSharpPostgresORM.SqlTypes.Boolean;
+
 namespace UnitTests;
 
 public class User
 {
-    [SqlColumn("PRIMARY KEY")] public BigSerial Id { get; set; }
+    [SqlColumn("PRIMARY KEY")]
+    public BigSerial Id { get; set; }
 
     [SqlColumn("NOT NULL"), ColumnLength(32)]
     public VarChar Name { get; set; }
 
-    public bool isTeacher { get; set; }
+    public Boolean isTeacher { get; set; }
 }
