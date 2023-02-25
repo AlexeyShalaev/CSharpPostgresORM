0b519db968dc8c06e4fe91bd491a3f1526fb4363
ChernV <vchern185@gmail.com>
2023-02-26 00:01:43 +0300

    SqlTypesTests: BigInteger & Character tests added

--- UnitTests/SqlTypesTests.cs
+++ UnitTests/SqlTypesTests.cs
@@ -1,4 +1,5 @@
 using PostgresORM.SqlTypes.Binary;
+using PostgresORM.SqlTypes.Numeric;
 using Boolean = PostgresORM.SqlTypes.Binary.Boolean;
 
 namespace UnitTests;
@@ -19,7 +20,7 @@ public class SqlTypesTests
         Assert.That((bool)(a & c), Is.False);
         Assert.That((bool)~a, Is.False);
         Assert.That((bool)~c, Is.True);
-        
+
         Assert.That((bool)(a ^ true), Is.False);
         Assert.That((bool)(a | false), Is.True);
         Assert.That((bool)(a & false), Is.False);
@@ -37,7 +38,7 @@ public class SqlTypesTests
         Assert.That((bool)(a & c), Is.False);
         Assert.That((bool)~a, Is.False);
         Assert.That((bool)~c, Is.True);
-        
+
         Assert.That((bool)(a ^ true), Is.False);
         Assert.That((bool)(a | false), Is.True);
         Assert.That((bool)(a & false), Is.False);
@@ -53,7 +54,7 @@ public class SqlTypesTests
         Assert.That((a ^ a) == new byte[] { 0, 0, 0, 0 }, Is.True);
         Assert.That((b & c) == new byte[] { 0b0101, 0b1010 }, Is.True);
         Assert.That((b | c) == new byte[] { 0b1111, 0b1111 }, Is.True);
-        
+
         Assert.That((a ^ new byte[] { 0, 255, 128, 1 }) == new byte[] { 0, 0, 0, 0 }, Is.True);
         Assert.That((b & new byte[] { 0b0101, 0b1111 }) == new byte[] { 0b0101, 0b1010 }, Is.True);
         Assert.That((b | new byte[] { 0b0101, 0b1111 }) == new byte[] { 0b1111, 0b1111 }, Is.True);
@@ -66,32 +67,60 @@ public class SqlTypesTests
     [Test]
     public void BigInteger()
     {
+        long a = 57;
+        long b = 179;
+
+        BigInteger a_ = a;
+        BigInteger b_ = b;
+
+        Assert.That((long)(a_ + b_), Is.EqualTo(a + b));
+        Assert.That((long)(a_ - b_), Is.EqualTo(a - b));
+        Assert.That((long)(a_ * b_), Is.EqualTo(a * b));
+        Assert.That((long)(a_ / b_), Is.EqualTo(a / b));
+        Assert.That((long)(b_ % a_), Is.EqualTo(b % a));
+        
+        Assert.That((long)++a_, Is.EqualTo(++a));
+        Assert.That((long)-a_, Is.EqualTo(-a));
+        Assert.That((long)--a_, Is.EqualTo(--a));
+
+        Assert.That(a_ > b_, Is.False);
+        Assert.That(a_ >= b_, Is.False);
+        Assert.That(a_ == b_, Is.False);
+        Assert.That(a_ <= b_, Is.True);
+        Assert.That(a_ < b_, Is.True);
     }
 
+    [Test]
     public void BigSerial()
     {
     }
 
+    [Test]
     public void Decimal()
     {
     }
 
+    [Test]
     public void Integer()
     {
     }
 
+    [Test]
     public void Numeric()
     {
     }
 
+    [Test]
     public void Real()
     {
     }
 
+    [Test]
     public void Serial()
     {
     }
 
+    [Test]
     public void SmallInteger()
     {
     }
@@ -103,6 +132,26 @@ public class SqlTypesTests
     [Test]
     public void Character()
     {
+        char a = 'a';
+        char b = 'z';
+
+        Character a_ = a;
+        Character b_ = b;
+
+        Assert.That((char)(a_ + b_), Is.EqualTo(a + b));
+        Assert.That((char)(b_ - a_), Is.EqualTo(b - a));
+        Assert.That((char)(a_ * b_), Is.EqualTo(a * b));
+        Assert.That((char)(a_ / b_), Is.EqualTo(a / b));
+        Assert.That((char)(b_ % a_), Is.EqualTo(b % a));
+        
+        Assert.That((char)++a_, Is.EqualTo(++a));
+        Assert.That((char)--a_, Is.EqualTo(--a));
+
+        Assert.That(a_ > b_, Is.False);
+        Assert.That(a_ >= b_, Is.False);
+        Assert.That(a_ == b_, Is.False);
+        Assert.That(a_ <= b_, Is.True);
+        Assert.That(a_ < b_, Is.True);
     }
 
     [Test]
